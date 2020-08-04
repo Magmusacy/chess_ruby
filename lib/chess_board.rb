0140a97ae3f431334809f348270f77d49ceb9c30
Magmusacy <xpfk.098@gmail.com>
2020-08-04 16:50:58 +0200

    removed comments

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -1,12 +1,7 @@
-# create chess board class
-# initialize it with vertical columns 1..8 and horizontal rows a..h
-# add an array of occupied squares to the initialize and update it with every move of the pieces
-
 class ChessBoard
 
   def initialize
     @board = Array("a".."h").product(Array(1..8))
     @occupied = []
   end
-  
 end
