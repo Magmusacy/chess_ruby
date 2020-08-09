ce3ab8b60c17583ec318eda9bf9b314b129073ef
Magmusacy <xpfk.098@gmail.com>
2020-08-09 13:58:01 +0200

    Finish knight class with its methods

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,4 +1,33 @@
 require './lib/pawn.rb'
 class Knight < Pawn
-  
+  def move(board, finish, available_moves = legal_moves(board))
+    super
+  end
+
+  def is_legal?(board, finish, legal = legal_moves(board))
+    super
+  end
+
+  private
+
+  def legal_moves(board, return_hash = {moves: []})
+    possible_moves = [
+    [position[0] + 2, (position[1].ord + 1).chr],
+    [position[0] + 2, (position[1].ord - 1).chr],
+    [position[0] - 2, (position[1].ord + 1).chr],
+    [position[0] - 2, (position[1].ord - 1).chr],
+    [position[0] + 1, (position[1].ord + 2).chr],
+    [position[0] + 1, (position[1].ord - 2).chr],
+    [position[0] - 1, (position[1].ord + 2).chr],
+    [position[0] - 1, (position[1].ord - 2).chr]
+    ]
+    board.each do |square|
+      if square.is_a?(Array) && possible_moves.include?(square)
+        return_hash[:moves] << square
+      elsif !square.is_a?(Array) && square.color != color && possible_moves.include?(square.position)
+        return_hash[:moves] << {square.position => square}
+      end
+    end
+    return_hash
+  end
 end
