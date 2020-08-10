02a1f0edb481b968e114d61172b49dd636b37b38
Magmusacy <xpfk.098@gmail.com>
2020-08-10 14:16:06 +0200

    Move common methods to module

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -1,5 +1,7 @@
 require './lib/pawn.rb'
+require './lib/common_methods.rb'
 class Bishop < Pawn
+  include CommonMethods
 
   def move(board, finish, available_moves = legal_moves(board))
     super
@@ -18,19 +20,6 @@ class Bishop < Pawn
        return_hash[:moves].concat check_diagonal(board, 1, -1, position)
        return_hash[:moves].concat check_diagonal(board, -1, 1, position)
     return_hash
-  end
-
-  def check_diagonal(board, character, number, curr_pos, moves = [])
-    return moves if moves.any? { |el| el.is_a?(Hash) }
-    next_pos = [curr_pos[0] + number, (curr_pos[1].ord + character).chr]
-    board.each do |square|
-      if square.is_a?(Array) && square == next_pos
-        moves << square
-      elsif !square.is_a?(Array) && square.position == next_pos
-        moves << {square.position => square}
-      end
-    end
-    check_diagonal(board, character, number, next_pos, moves)
   end
 
 end
