d39c3b764c233e3bbcdfd1f2877c711545637459
Magmusacy <xpfk.098@gmail.com>
2020-08-11 17:08:05 +0200

    Create king class with its methods

--- lib/king.rb
+++ lib/king.rb
@@ -1,4 +1,43 @@
 require './lib/pawn.rb'
 class King < Pawn
-  
+  def move(board, finish, available_moves = legal_moves(board))
+    super
+  end
+
+  def is_legal?(board, finish, legal = legal_moves(board))
+    super
+  end
+
+  private
+
+  def legal_moves(board) 
+    return_hash = {moves: []}
+    board.each do |square|
+      if square.is_a?(Array)
+        return_hash[:moves] << square if find_move_row(square)
+      elsif !square.is_a?(Array)
+        return_hash[:moves] << {square => square.position} if find_move_row(square.position) && square.color != color
+      end
+    end
+    return_hash
+  end
+
+  def find_move_row(square) 
+    case square[0] - position[0]
+    when -1 # upper row
+      find_move_column(square)
+    when 1 # lower row
+      find_move_column(square)
+    when 0 # current row
+      find_move_column(square)
+    else
+      false
+    end
+  end
+
+  def find_move_column(square)
+    return true if (square[1].ord + 1).chr == position[1] || (square[1].ord - 1).chr == position[1] || square[1] == position[1]
+    false
+  end
+
 end
