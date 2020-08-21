c4d847ed0fbd03a71e01532628a76117524b7486
Magmusacy <xpfk.098@gmail.com>
2020-08-21 12:23:58 +0200

    Add beats_king? method

--- lib/common_methods.rb
+++ lib/common_methods.rb
@@ -4,6 +4,7 @@ module CommonMethods
     next_pos = [curr_pos[0] + number, (curr_pos[1].ord + character).chr]
     return moves if moves.any? { |el| el.is_a?(Hash) } || !Array(1..8).product(Array('a'..'h')).include?(next_pos)
     board.each do |square|
+      return moves if !square.is_a?(Array) && square.position == next_pos && square.color == self.color
       if square.is_a?(Array) && square == next_pos
         moves << square
       elsif !square.is_a?(Array) && square.position == next_pos && square.color != self.color
@@ -16,11 +17,10 @@ module CommonMethods
   def search_moves(board, direction, return_hash = {moves: []})
     current_row = board.select{ |square| (square.is_a?(Array) && square[direction] == position[direction]) || (!square.is_a?(Array) && square.position[direction] == position[direction])}
     self_index = current_row.index(self)
-    left_part = current_row[0..self_index].reverse - [self] # left part of the array starting from self position
-    right_part = current_row[self_index..-1] - [self] # left part of the array starting from self position
+    left_part = current_row[0...self_index].reverse # left part of the array starting from self position
+    right_part = current_row[self_index+1..-1] # left part of the array starting from self position
     return_hash = find_path(left_part, {moves: []})
     return_hash = find_path(right_part, return_hash)
-    p return_hash
   end
 
   def find_path(array, return_hash)
@@ -33,6 +33,11 @@ module CommonMethods
       end 
     end
     return_hash
+  end
+
+  def beats_king?(ary)
+    ary.each { |move| return {true => move.first[0]} if move.is_a?(Hash) && move.first[1].is_a?(King) } 
+    false
   end
 
 end
