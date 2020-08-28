6c756f8ccb42e8b255d1250db73011dff144535f
Magmusacy <xpfk.098@gmail.com>
2020-08-28 19:17:40 +0200

    Add ability for king to castle

--- lib/king.rb
+++ lib/king.rb
@@ -2,7 +2,7 @@ require './lib/pawn.rb'
 require './lib/chess_board.rb'
 class King < Pawn
   include CommonMethods
-  def legal_moves(board, illegal=false) # these are all possible moves 
+  def legal_moves(board, illegal=false, in_check=false) # these are all possible moves 
     return_hash = {moves: []}
     board.each do |square|
       if square.is_a?(Array)
@@ -12,7 +12,9 @@ class King < Pawn
       end
     end
     return return_hash if illegal
-    remove_illegal_moves(return_hash, board) # remove moves after which the king is in check
+    correct_moves = remove_illegal_moves(return_hash, board) # remove moves after which the king is in check
+    correct_moves[:moves].concat(castling(board)[:moves]) 
+    correct_moves
   end
 
   private
@@ -56,11 +58,56 @@ class King < Pawn
     chess.assign_board_squares(opponent)
     chess.board = king.move(chess.board, new_move, legal_moves(chess.board, true))
 
+    return king_in_check?(opponent, chess.board) 
+  end
+
+  def king_in_check?(opponent, board)
     opponent.pieces.each do |piece|
-      legal = piece.is_a?(King) ? beats_king?(piece.legal_moves(chess.board, true)[:moves]) : beats_king?(piece.legal_moves(chess.board)[:moves])
+      legal = piece.is_a?(King) ? beats_king?(piece.legal_moves(board, true)[:moves]) : beats_king?(piece.legal_moves(board)[:moves])
       return legal unless legal == false # return true next move is checked
     end
-    return false # return false  next move isn't checked
+    false
+  end
+
+  def castling(board, return_hash = {moves: []})
+    rooks = board.select { |piece| piece.is_a?(Rook) && piece.color == color && piece.last_move.empty? }
+    king_check = is_attacked?(board, self) == false ? false : true
+    if last_move.empty? && !king_check
+        rooks.each do |rook|
+          king_col = self.position[1].ord
+          rook_col = rook.position[1].ord
+          free_path = king_col > rook_col ? free_space?(rook, board, king_col, rook_col) : free_space?(rook, board, rook_col, king_col)
+          king_move = calculate_new_position(king_col, rook_col, 2)
+          rook_move = calculate_new_position(king_col, rook_col, 1)
+          return_hash[:moves] << {king_move => [self, rook_move, rook]} if free_path
+        end 
+    end
+    return_hash
+  end
+
+  def calculate_new_position(king_col, rook_col, move)
+    diff = (king_col - rook_col).abs - 1
+    diff == 3 ? [self.position[0], (king_col - move).chr] : [self.position[0], (king_col + move).chr]
+  end 
+
+  def free_space?(rook, board, minuend, subtrahend, ary=[]) 
+    board_free_spaces = board.select{ |square| square.is_a?(Array) }
+    count = minuend - subtrahend-1
+      1.upto(count) do |i|
+        ary << !is_attacked?(board, [self.position[0], (subtrahend + i).chr]) unless (count == 3 && i == 1) 
+        ary << board_free_spaces.include?([self.position[0], (subtrahend + i).chr]) 
+      end
+    ary.all?{ |el| el == true }
+  end
+
+  def is_attacked?(board, square)
+    opponent = board.find{ |sqr| sqr.is_a?(King) && sqr != self }.parent
+    return king_in_check?(opponent, board) if square == self
+    opponent.pieces.each do |piece|
+      legal = piece.is_a?(King) ? piece.legal_moves(board, true)[:moves] : piece.legal_moves(board)[:moves]
+      return true if legal.include?(square)
+    end
+    false
   end
 
 end
